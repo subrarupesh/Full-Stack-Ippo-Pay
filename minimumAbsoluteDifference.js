59914814a2e3ab70be01e65db0b4ec4b483e788a
subrarupesh <subrarupesh@gmail.com>
2023-05-18 20:06:44 +0530

    Create minimumAbsoluteDifference.js

---
+++ minimumAbsoluteDifference.js
@@ -0,0 +1,30 @@
+minAbsDiff = (arr) => {
+           
+        arr.sort(function(a, b){return a - b});
+           
+
+        let sum = 0;
+           
+        let n = arr.length; 
+       
+        sum += (arr[0] - arr[1]);
+        if(n === 2) {
+            return Math.abs(sum);
+        }   
+        
+        sum += (arr[n-1] - arr[n-2]);
+        if(n === 4) {
+            return Math.abs(sum);
+        }
+         
+         
+        for (let i = 1; i < n-1; i++)
+            sum += Math.min(Math.abs(arr[i] - arr[i-1]),
+                    Math.abs(arr[i] - arr[i+1]));
+               
+        return sum;
+    }
+     
+    let arr = [2,-1,0,4,-2,-9];
+ 
+    console.log(minAbsDiff(arr));
